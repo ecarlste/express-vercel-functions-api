bbcdbb133a3dfa5432701989e2cd4d52e6fa709b
Erik S. Carlsten <ecarlste@gmail.com>
2025-06-19 14:29:02 -0600

    generate drizzle schema migration for category entity

---
+++ drizzle/0000_illegal_ink.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `categories` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text,
+	`created_at` integer DEFAULT (strftime('%s', 'now')) NOT NULL,
+	`updated_at` integer DEFAULT (strftime('%s', 'now')) NOT NULL
+);
